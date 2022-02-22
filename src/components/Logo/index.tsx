3e0d5f991f1d99ca237174c81f306c4ea7a905ca
Matheus Pedroni <mpedroni77@gmail.com>
2022-02-22 14:19:34 -0300

    Edit app logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,10 @@
 import styles from "./styles.module.scss";
-import logoSvg from "../../../public/logo.svg";
 import Image from "next/image";
 
 export function Logo() {
   return (
     <div className={styles.container}>
-      <Image src={logoSvg} alt="h.analyzer logo" />
+      <Image src="/logo.svg" width={32} height={32} alt="h.analyzer logo" />
       <h1 className={styles.logo}>
         h.<span>analyzer</span>
       </h1>
